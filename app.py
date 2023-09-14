82e7eba68842ba353f7b5994955074dc55424bf4
Pengcaiji <91669791+NiuLearner@users.noreply.github.com>
2023-09-14 15:34:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,30 +5,50 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 
-data_url = (
-    "https://raw.githubusercontent.com/NiuLearner/dashPython/main/Stocks.csv"
-)
+data_url = "https://raw.githubusercontent.com/NiuLearner/dashPython/main/Stocks.csv"
 df_all = pd.read_csv(data_url)
-# initial Dash
+
+available_tickers = df_all['Ticker'].unique()
+
 app = dash.Dash(__name__)
 server = app.server
-# figures
-fig_price = px.line(df_all, x='Date', y='Close',
-                    color='Ticker', title='Stock Price Over Time')
-fig_volume = px.bar(df_all, x='Date', y='Volume',
-                    color='Ticker', title='Stock Volume Over Time')
-fig_stock_share = px.pie(df_all.groupby('Ticker').agg({'Close': 'mean'}).reset_index(
-), values='Close', names='Ticker', title='Market Share by Stock')
 
-
-
-# Dash outlayer
 app.layout = html.Div([
     html.H1("Financial Dashboard"),
-    dcc.Graph(id='price_graph', figure=fig_price),
-    dcc.Graph(id='volume_graph', figure=fig_volume),
-    dcc.Graph(id='stock_share_graph', figure=fig_stock_share)
+    
+
+    html.Label("Select Stock Tickers:"),
+    dcc.Dropdown(
+        id='ticker_dropdown',
+        options=[{'label': i, 'value': i} for i in available_tickers],
+        value=available_tickers, 
+        multi=True  
+    ),
+    
+    dcc.Graph(id='price_graph'),
+    dcc.Graph(id='volume_graph'),
+    dcc.Graph(id='stock_share_graph')
 ])
+
+@app.callback(
+    [Output('price_graph', 'figure'),
+     Output('volume_graph', 'figure'),
+     Output('stock_share_graph', 'figure')],
+    [Input('ticker_dropdown', 'value')]
+)
+def update_graph(selected_tickers):
+    filtered_df = df_all[df_all['Ticker'].isin(selected_tickers)]
+    
+    fig_price = px.line(filtered_df, x='Date', y='Close',
+                        color='Ticker', title='Stock Price Over Time')
+    
+    fig_volume = px.bar(filtered_df, x='Date', y='Volume',
+                        color='Ticker', title='Stock Volume Over Time')
+    
+    fig_stock_share = px.pie(filtered_df.groupby('Ticker').agg({'Close': 'mean'}).reset_index(),
+                             values='Close', names='Ticker', title='Market Share by Stock')
+    
+    return fig_price, fig_volume, fig_stock_share
 
 if __name__ == '__main__':
     app.run_server(debug=True)
